faa6566a08878072c5f1fc26666551b311c20999
Roman Himmelreich <himmelroman@gmail.com>
2024-02-27 21:18:47 +0200

    faster

--- test/ffmpeg/blend/blend.sh
+++ test/ffmpeg/blend/blend.sh
@@ -3,4 +3,4 @@
 #ffmpeg -framerate 1/2 -pattern_type glob -i "*.png" -vf "minterpolate=fps=25:mi_mode=mci:mc_mode=aobmc:me_mode=bidir:vsbmc=0.9, unsharp=luma_msize_x=7:luma_msize_y=7:luma_amount=2.5" out.mp4
 ffmpeg -framerate 1/3 -pattern_type glob -i "*.png" -vf "minterpolate=fps=12:mi_mode=mci:mc_mode=aobmc:me_mode=bidir:vsbmc=0.9, cas=0.9" out.mp4
 
-ffmpeg -hwaccel cuda -hwaccel_output_format cuda -pattern_type glob -i "*.png" -c:v h264_nvenc out.mp4
+# ffmpeg -hwaccel cuda -hwaccel_output_format cuda -pattern_type glob -i "*.png" -c:v h264_nvenc out.mp4
