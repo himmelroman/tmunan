4e8d616fe65091f9e7a78fb15877e4eb88c8dc94
Roman Himmelreich <himmelroman@gmail.com>
2024-03-02 16:56:15 +0200

    retry hls.js

--- tmunan/ui/scripts/video.js
+++ tmunan/ui/scripts/video.js
@@ -1,5 +1,5 @@
 MAX_RETRY_ATTEMPTS = 100    // retry 100 times
-RETRY_DELAY = 1000          // one second delay between attempts
+RETRY_DELAY = 1000          // one-second delay between attempts
 
 function playVideo(video_element, video_url) {
 
@@ -24,14 +24,13 @@ function playVideo(video_element, video_url) {
               },
             },
         });
-
-        hls.attachMedia(video_element);
         hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+            console.log('media attached!');
             video_element.muted = true;
             video_element.play();
         });
         hls.on(Hls.Events.MANIFEST_PARSED, function () {
-            console.log('manifest parsed');
+            console.log('manifest parsed!');
             hls.current?.play();
         });
         hls.on(Hls.Events.ERROR, function (event, data) {
@@ -43,26 +42,14 @@ function playVideo(video_element, video_url) {
               setTimeout(() => {
                 hls.loadSource(hls.source); // Retry loading the manifest
               }, RETRY_DELAY);
-              hls.loadSource(hls.source); // Retry loading the manifest
             } else {
               console.error(`Manifest fetch failed after ${MAX_RETRY_ATTEMPTS} attempts. Giving up.`);
             }
           }
         });
+        hls.attachMedia(video_element);
         hls.loadSource(video_url);
     }
-
-    // hls.on(Hls.Events.ERROR, (event, data) => {
-    //   if (data.details === Hls.ErrorDetails.NETWORK_ERROR && data.fatal === false) {
-    //     const currentAttempt = data.retry + 1; // Extract current attempt from data
-    //     if (currentAttempt <= MAX_RETRY_ATTEMPTS) {
-    //       console.warn(`Manifest fetch failed (attempt ${currentAttempt}/${MAX_RETRY_ATTEMPTS}), retrying...`);
-    //       hls.loadSource(hls.source); // Retry loading the manifest
-    //     } else {
-    //       console.error(`Manifest fetch failed after ${MAX_RETRY_ATTEMPTS} attempts. Giving up.`);
-    //     }
-    //   }
-    // });
 
     // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
     // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element through the `src` property.
