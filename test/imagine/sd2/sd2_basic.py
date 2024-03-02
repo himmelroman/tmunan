7690bfed30853568a0e24603092c945b459f0826
Roman Himmelreich <himmelroman@gmail.com>
2024-03-02 17:28:59 +0200

    stable diff 2.1

---
+++ test/imagine/sd2/sd2_basic.py
@@ -0,0 +1,19 @@
+import time
+import torch
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+
+model_id = "stabilityai/stable-diffusion-2-1"
+
+# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+
+start_time = time.time()
+image = pipe(prompt).images[0]
+elapsed_time = time.time() - start_time
+print(f'Elapsed time: {elapsed_time}')
+
+image.save("astronaut_rides_horse.png")
