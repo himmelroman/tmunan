7829437e202865151128282d87dd2719c8d886dc
Roman Himmelreich <himmelroman@gmail.com>
2024-03-14 20:16:38 +0200

    switch to blend

--- test/latent_blending/test.py
+++ test/latent_blending/test.py
@@ -1,9 +1,43 @@
 import torch
 import warnings
-from tqdm import tqdm
+
+from compel import Compel, ReturnedEmbeddingsType
+
 from diffusers import AutoPipelineForText2Image
 from latentblending import add_frames_linear_interp
 from latentblending.blending_engine import BlendingEngine
+
+
+def get_text_embedding(self, prompt):
+
+    print('My embeddings are running!')
+    compel = Compel(
+                    tokenizer=[self.pipe.tokenizer, self.pipe.tokenizer_2],
+                    text_encoder=[self.pipe.text_encoder, self.pipe.text_encoder_2],
+                    returned_embeddings_type=ReturnedEmbeddingsType.PENULTIMATE_HIDDEN_STATES_NON_NORMALIZED,
+                    requires_pooled=[False, True]
+                )
+
+    # create compel prompt components
+    conditioning, pooled = compel(prompt)
+
+    do_classifier_free_guidance = self.guidance_scale > 1 and self.pipe.unet.config.time_cond_proj_dim is None
+    text_embeddings = self.pipe.encode_prompt(
+        # prompt=prompt,
+        # prompt_2=prompt,
+        device=self.pipe._execution_device,
+        num_images_per_prompt=1,
+        do_classifier_free_guidance=do_classifier_free_guidance,
+        negative_prompt=self.negative_prompt,
+        negative_prompt_2=self.negative_prompt,
+        prompt_embeds=conditioning,
+        pooled_prompt_embeds=pooled,
+        negative_prompt_embeds=None,
+        negative_pooled_prompt_embeds=None,
+        lora_scale=None,
+        clip_skip=None,  # self.pipe._clip_skip,
+    )
+    return text_embeddings
 
 
 warnings.filterwarnings('ignore')
@@ -15,6 +49,7 @@ pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch
 pipe.to("mps")
 
 be = BlendingEngine(pipe)
+be.get_text_embeddings = get_text_embedding
 be.set_prompt1("lightly cloudy sky background, (pink clouds)0.1")
 be.set_prompt2("(lightly cloudy sky background)0.3, (pink clouds)1.0")
 be.set_negative_prompt("blurry, ugly, pale")
