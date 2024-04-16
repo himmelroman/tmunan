86b7710ccda477a86c6d4c514eef39de498ab34d
Roman Himmelreich <himmelroman@gmail.com>
2024-04-17 01:41:28 +0300

    read model size from env

--- tmunan/imagine/imagine_app.py
+++ tmunan/imagine/imagine_app.py
@@ -33,8 +33,8 @@ class AppSettings(BaseSettings):
 async def lifespan(fastapi_app: FastAPI):
 
     # determine model size
-    cuda_model_size = os.environ['CUDA_MODEL_SIZE'] or 'large'
-    other_model_size = os.environ['OTHER_MODEL_SIZE'] or 'small'
+    cuda_model_size = os.environ.get('CUDA_MODEL_SIZE') or 'large'
+    other_model_size = os.environ.get('OTHER_MODEL_SIZE') or 'small'
     model_size = cuda_model_size if torch.cuda.is_available() else other_model_size
 
     # LCM
