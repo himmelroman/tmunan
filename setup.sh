876616dac313e11b3945cef0cf5e2672a89d512b
Roman Himmelreich <himmelroman@gmail.com>
2024-04-17 14:57:37 +0300

    ec2 setup bash

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@ cd tmunan
 pip install -r requirements.txt
 
 screen -R tmunan
+mkdir -p /home/ubuntu/.cache/theatre/imagine/rubin_style
+curl https://b5ff-62-56-134-6.ngrok-free.app/rubin3.jpg > /home/ubuntu/.cache/theatre/imagine/rubin_style/rubin3.jpg
 CUDA_MODEL_SIZE=small uvicorn tmunan.imagine.imagine_app:app --host 0.0.0.0 --port 8080
