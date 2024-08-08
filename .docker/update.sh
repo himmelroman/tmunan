1bc67617a3ad490ba506435e9fe5bbcfc7e67730
Roman Himmelreich <himmelroman@gmail.com>
2024-08-08 17:41:45 +0300

    update script

---
+++ .docker/update.sh
@@ -0,0 +1,6 @@
+docker-compose down
+cd ../tmunan
+docker build -t himmelroman/stream_app -f stream_app/Dockerfile .
+docker build -t himmelroman/imagine_app -f imagine_app/Dockerfile .
+cd .docker
+docker-compose uפ
