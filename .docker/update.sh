d7c8b63bedc5de7f2494446303387e003747494d
Roman Himmelreich <himmelroman@gmail.com>
2024-08-10 12:27:43 +0300

    fix update.sh

--- .docker/update.sh
+++ .docker/update.sh
@@ -2,6 +2,6 @@ docker-compose down
 cd ../tmunan
 git pull
 docker build -t himmelroman/tmunan_stream:latest -f stream_app/Dockerfile .
-docker build -t himmelroman/tmunan_image:latest -f imagine_app/Dockerfile .
+docker build -t himmelroman/tmunan_imagine:latest -f imagine_app/Dockerfile .
 cd ../.docker
 docker-compose up
