b0ab9f42681990ec3862c862d22a59945a415dc6
Roman Himmelreich <himmelroman@gmail.com>
2024-08-13 15:21:53 +0300

    logs with milliseconds

--- tmunan/utils/log.py
+++ tmunan/utils/log.py
@@ -1,20 +1,25 @@
 import os
 import logging
 from pathlib import Path
-from logging import DEBUG, INFO
+from logging import INFO
 
 # Add global log levels
 STDOUT = INFO + 1
 STDERR = INFO + 2
 logging.addLevelName(STDOUT, 'STDOUT')
 logging.addLevelName(STDERR, 'STDERR')
-logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+# logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(format='%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S',)
 
 # constants
 LOG_DIR = Path(os.environ['WORK_DIR'] if 'WORK_DIR' in os.environ else '/tmp/') / 'log'
 
 
 def get_logger(log_name, log_path=None, console=True):
+
+    # read log level
+    log_level = os.environ.get('LOG_LEVEL', 'INFO')
+    log_level = logging.getLevelNamesMapping()[log_level]
 
     # init log dir
     if not log_path:
@@ -24,14 +29,14 @@ def get_logger(log_name, log_path=None, console=True):
     logger = logging.getLogger(log_name)
 
     # set logger level
-    logger.setLevel(DEBUG)
+    logger.setLevel(log_level)
 
     # create console handler, if needed
     if console and not os.environ.get('PYTHON_LOG_DISABLE_CONSOLE', False):
 
         # create log handler
         ch = logging.StreamHandler()
-        ch.setLevel(INFO)
+        ch.setLevel(log_level)
 
         # Add console handler to root logger
         # Other packages might register a StreamHandler when calling logging.basicConfig() (e.g. rollbar)
@@ -46,7 +51,7 @@ def get_logger(log_name, log_path=None, console=True):
 
         # init file handle
         fh = logging.FileHandler(log_path, encoding='utf-8')
-        fh.setLevel(DEBUG)
+        fh.setLevel(log_level)
 
         # add handler to logger
         logger.addHandler(fh)
